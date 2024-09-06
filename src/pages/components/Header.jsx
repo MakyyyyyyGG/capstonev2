ffa9b3659e259961761baff4d46163a1bd394c9f
MarkBit109 <markbit109@gmail.com>
2024-09-06 22:19:16 +0800

    NavBar Beta ver

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -1,10 +1,44 @@
 import React, { useState, useEffect } from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
-import { Button, Chip } from "@nextui-org/react";
+import { Button, ButtonGroup, Chip } from "@nextui-org/react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Input,
+  Select,
+  SelectItem,
+  DropdownSection,
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+  Avatar,
+} from "@nextui-org/react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from "@nextui-org/react";
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/react";
 import { useRouter } from "next/router";
 import JoinRoom from "./JoinRoom";
 const Header = () => {
   const router = useRouter();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isFollowed, setIsFollowed] = React.useState(false);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  const handleOpenModal = () => {
+    setIsDropdownOpen(false);
+    onOpen();
+  };
+
   const { data: session, status } = useSession();
   const [isEditing, setIsEditing] = useState(false);
   const [isLocationEditing, setIsLocationEditing] = useState(false);
@@ -421,7 +455,409 @@ const Header = () => {
 
   return (
     <div className="flex justify-between">
-      <p>username {userData?.first_name}</p>
+      <Navbar isBordered>
+        <NavbarContent justify="start">
+          <NavbarBrand className="mr-4">
+            <p className="hidden sm:block font-bold text-inherit">LNK</p>
+          </NavbarBrand>
+        </NavbarContent>
+        <NavbarContent as="div" className="items-center" justify="end">
+          <Dropdown
+            isOpen={isDropdownOpen}
+            onOpenChange={setIsDropdownOpen}
+            placement="bottom-end"
+          >
+            <DropdownTrigger>
+              <Avatar
+                isBordered
+                as="button"
+                className="transition-transform"
+                color="secondary"
+                name="Jason Hughes"
+                size="md"
+                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+              />
+            </DropdownTrigger>
+            <DropdownMenu
+              aria-label="Profile Actions"
+              variant="flat"
+              className="min-w-[380px]"
+            >
+              <DropdownSection showDivider>
+                <DropdownItem
+                  isReadOnly
+                  key="profile"
+                  className="h-14 gap-2 text-center cursor-default"
+                >
+                  <p className="font-semibold">Signed in as</p>
+                  <p className="font-light">{session.user.email}</p>
+                </DropdownItem>
+                <DropdownItem
+                  isReadOnly
+                  key="profile"
+                  className="h-30 gap-2 cursor-default flex justify-center"
+                >
+                  <div className="flex justify-center w-full">
+                    <Avatar
+                      src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                      className="w-[100px] h-[100px] text-large"
+                    />
+                  </div>
+                </DropdownItem>
+                <DropdownItem
+                  isReadOnly
+                  key="profile"
+                  className="h-30 gap-2 cursor-default"
+                >
+                  <p className=" text-center text-2xl">Hi, {firstName}!</p>
+                </DropdownItem>
+              </DropdownSection>
+              <DropdownSection>
+                <DropdownItem
+                  isReadOnly
+                  key="settings"
+                  className="min-w-[100px] flex justify-center gap-2 cursor-default"
+                >
+                  <div className="flex flex-row gap-2">
+                    <Button
+                      onPress={handleOpenModal}
+                      radius="full"
+                      size="sm"
+                      className="w-full"
+                    >
+                      Manage Account
+                    </Button>
+                    <Button
+                      color="danger"
+                      radius="full"
+                      size="sm"
+                      className="w-full"
+                      onPress={() =>
+                        signOut({ redirect: false }).then(() =>
+                          router.push("/")
+                        )
+                      }
+                    >
+                      Signout
+                    </Button>
+                  </div>
+                </DropdownItem>
+              </DropdownSection>
+            </DropdownMenu>
+          </Dropdown>
+        </NavbarContent>
+      </Navbar>
+
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        backdrop="opaque"
+        size="2xl"
+        radius="lg"
+        placement="center"
+        classNames={{
+          body: "py-6",
+          backdrop: "bg-[#292f46]/50 backdrop-opacity-40",
+          base: "border-[#292f46] bg-[#fff] text-[#000]",
+          header: "border-b-[1px] border-gray",
+          footer: "border-t-[1px] border-gray",
+          closeButton: "hover:bg-purple-700/5 active:bg-purple-700/10",
+        }}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Profile Details
+              </ModalHeader>
+              <ModalBody className="mx-2 grid grid-cols-7 gap-3 justify-between">
+                <div className="col-span-1">
+                  <p>Profile</p>
+                </div>
+                <div className="col-span-4 col-start-3 col-end-8">
+                  {isEditing ? (
+                    <>
+                      <Card className="w-full p-2">
+                        <CardHeader>
+                          <div className="flex gap-5">
+                            <img
+                              src={`${profileImage}?${new Date().getTime()}`}
+                              alt="User Profile"
+                              style={{
+                                width: "70px",
+                                height: "70px",
+                                borderRadius: "50%",
+                                border: "1px solid #ccc",
+                              }}
+                            />
+                            <div className="flex flex-col justify-center text-sm">
+                              <input
+                                type="file"
+                                accept="image/*"
+                                onChange={handleImageChange}
+                              />
+                            </div>
+                          </div>
+                        </CardHeader>
+                        <CardBody className="py-3 text-small text-default-400 font-semibold">
+                          <div className="grid grid-cols-2 gap-3">
+                            <Input
+                              type="text"
+                              label="First Name"
+                              size="sm"
+                              variant="bordered"
+                              value={firstName || ""}
+                              onChange={(e) => setFirstName(e.target.value)}
+                            />
+                            <Input
+                              type="text"
+                              label="Last Name"
+                              size="sm"
+                              variant="bordered"
+                              value={lastName || ""}
+                              onChange={(e) => setLastName(e.target.value)}
+                            />
+                            <Input
+                              type="number"
+                              label="Age"
+                              size="sm"
+                              variant="bordered"
+                              value={age || ""}
+                              onChange={(e) => setAge(e.target.value)}
+                            />
+                            <Select
+                              name="gender"
+                              id="gender"
+                              label="Choose your Gender"
+                              placeholder="Select Gender"
+                              size="sm"
+                              variant="bordered"
+                              value={gender || ""}
+                              onChange={(e) => setGender(e.target.value)}
+                            >
+                              <SelectItem value="Male">Male</SelectItem>
+                              <SelectItem value="Female">Female</SelectItem>
+                              <SelectItem value="Other">Other</SelectItem>
+                            </Select>
+                            <label>Choose a gender:</label>
+                            <select
+                              name="gender"
+                              id="gender"
+                              value={gender || ""}
+                              onChange={(e) => setGender(e.target.value)}
+                            >
+                              <option value="">Select Gender</option>
+                              <option value="Male">Male</option>
+                              <option value="Female">Female</option>
+                              <option value="Other">Other</option>
+                            </select>
+                            <Input //DatePicker has some problems
+                              className="col-span-2"
+                              type="date"
+                              label="Enter Your Birth Date"
+                              size="sm"
+                              variant="bordered"
+                              value={bday || ""}
+                              onChange={(e) => setBday(e.target.value)}
+                            />
+                          </div>
+                        </CardBody>
+                        <CardFooter className="flex justify-end gap-3">
+                          <Button
+                            color="default"
+                            size="sm"
+                            onClick={handleCancelClick}
+                          >
+                            Cancel
+                          </Button>
+                          <Button
+                            color="secondary"
+                            size="sm"
+                            onClick={handleSaveClick}
+                          >
+                            Save
+                          </Button>
+                        </CardFooter>
+                      </Card>
+                    </>
+                  ) : (
+                    <>
+                      <div className="min-w-[400px] flex flex-row items-center justify-between gap-5 text-sm">
+                        <div className="flex flex-col items-center gap-3">
+                          <div className="flex flex-row items-center gap-3">
+                            <img
+                              src={`${profileImage}?${new Date().getTime()}`}
+                              alt="User Profile"
+                              style={{
+                                width: "70px",
+                                height: "70px",
+                                borderRadius: "50%",
+                                border: "1px solid #ccc",
+                              }}
+                            />
+                            <p>
+                              {firstName} {lastName}
+                            </p>
+                          </div>
+                          <div>
+                            <p>Age: {age}</p>
+                            <p>Gender: {gender}</p>
+                            <p>Birthday: {bday}</p>
+                          </div>
+                        </div>
+
+                        <Button
+                          color="secondary"
+                          size="sm"
+                          radius="full"
+                          className="px-4"
+                          onClick={handleUpdateClick}
+                        >
+                          Update
+                        </Button>
+                      </div>
+                    </>
+                  )}
+                </div>
+              </ModalBody>
+              <hr className="mx-8 border-gray opacity-75" />
+              <ModalBody className="mx-2 grid grid-cols-7 gap-3 justify-between">
+                <div className="col-span-1">
+                  <p>Location</p>
+                </div>
+                <div className="col-span-4 col-start-3 col-end-8">
+                  {isLocationEditing ? (
+                    <>
+                      <Card className="w-full p-2">
+                        <CardHeader>
+                          <div className="flex gap-5">
+                            <h4 className="text-small font-semibold leading-none text-default-600">
+                              Edit Your Location
+                            </h4>
+                          </div>
+                        </CardHeader>
+                        <CardBody className="px-3 py-0 text-small text-default-400">
+                          <label>Region:</label>
+                          <select
+                            name="region"
+                            id="region"
+                            value={selectedRegion || ""}
+                            onChange={handleRegionChange}
+                          >
+                            <option value="">Select Region</option>
+                            {regions.map((region) => (
+                              <option key={region.code} value={region.code}>
+                                {region.name}
+                              </option>
+                            ))}
+                          </select>
+
+                          <label>Province:</label>
+                          <select
+                            value={selectedProvince || ""}
+                            onChange={handleProvinceChange}
+                          >
+                            <option value="">Select Province</option>
+                            {provinces.map((province) => (
+                              <option key={province.code} value={province.code}>
+                                {province.name}
+                              </option>
+                            ))}
+                          </select>
+
+                          <label>Municipality:</label>
+                          <select
+                            value={selectedMunicipality || ""}
+                            onChange={handleMunicipalityChange}
+                          >
+                            <option value="">Select Municipality</option>
+                            {selectedProvinceText === "Albay" && (
+                              <option value="Legazpi">Legazpi</option>
+                            )}
+                            {municipalities.map((municipality) => (
+                              <option
+                                key={municipality.code}
+                                value={municipality.code}
+                              >
+                                {municipality.name}
+                              </option>
+                            ))}
+                          </select>
+
+                          <label>Barangay:</label>
+                          <select
+                            value={selectedBarangay || ""}
+                            onChange={handleBarangayChange}
+                          >
+                            <option value="">Select Barangay</option>
+                            {selectedMunicipality === "Legazpi"
+                              ? customBarangays.map((barangay, index) => (
+                                  <option key={index} value={barangay}>
+                                    {barangay}
+                                  </option>
+                                ))
+                              : barangays.map((barangay) => (
+                                  <option
+                                    key={barangay.code}
+                                    value={barangay.code}
+                                  >
+                                    {barangay.name}
+                                  </option>
+                                ))}
+                          </select>
+                        </CardBody>
+                        <CardFooter className="flex justify-end gap-3">
+                          <Button
+                            color="default"
+                            size="sm"
+                            onClick={handleCancelClick}
+                          >
+                            Cancel
+                          </Button>
+                          <Button
+                            color="secondary"
+                            size="sm"
+                            onClick={handleLocationSaveClick}
+                          >
+                            Save
+                          </Button>
+                        </CardFooter>
+                      </Card>
+                    </>
+                  ) : (
+                    <>
+                      <div className="min-w-[400px] flex flex-row items-center justify-between gap-5 text-sm">
+                        <div>
+                          <p>Region: {userData?.region}</p>
+                          <p>Province: {userData?.province}</p>
+                          <p>Municipality: {userData?.municipality}</p>
+                          <p>Barangay: {userData?.barangay}</p>
+                        </div>
+                        <Button
+                          color="secondary"
+                          size="sm"
+                          radius="full"
+                          className="px-4"
+                          onClick={handleLocUpdateClick}
+                        >
+                          Update
+                        </Button>
+                      </div>
+                    </>
+                  )}
+                </div>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+
+      {/* <p>username {userData?.first_name}</p>
       <p>Welcome, {session.user.email}!</p>
       <p>Your name is {session.user.name}</p>
       <p>Your user ID is: {session.user.id}</p>
@@ -590,7 +1026,7 @@ const Header = () => {
         <p>province {userData?.province}</p>
         <p>municipality {userData?.municipality}</p>
         <p>barangay {userData?.barangay}</p>
-      </div>
+      </div> */}
     </div>
   );
 };
