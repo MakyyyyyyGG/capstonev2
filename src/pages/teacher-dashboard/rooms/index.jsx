f1bf086832283ce00149c382a5d8cfa3d9127d84
MarkBit109 <markbit109@gmail.com>
2024-10-09 01:03:54 +0800

    Room Card is clickable

--- src/pages/teacher-dashboard/rooms/index.jsx
+++ src/pages/teacher-dashboard/rooms/index.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import Link from "next/link";
-import { Button, Input, Card, CardFooter, Chip } from "@nextui-org/react";
+import {
+  Button,
+  Input,
+  Card,
+  CardHeader,
+  CardFooter,
+  Chip,
+} from "@nextui-org/react";
 import DeleteRoom from "@/pages/components/DeleteRoom";
 
 const Rooms = ({ rooms, onRoomDeleted }) => {
@@ -45,8 +52,12 @@ const Rooms = ({ rooms, onRoomDeleted }) => {
             key={room.room_id}
             className="shrink w-[380px] h-[300px] bg-[#7469B6] grid grid-rows-7 hover:shadow-gray-400 shadow-lg"
           >
-            <div className="p-5 row-span-5 grid content-center">
-              <div className="absolute">
+            <CardHeader
+              href={`/teacher-dashboard/rooms/${room.room_code}`}
+              as={Link}
+              className="relative w-full p-5 row-span-5 items-center text-center"
+            >
+              <div className="absolute top-0 left-0 p-5">
                 <Chip
                   color={getChipColor(room.room_difficulty)}
                   radius="sm"
@@ -55,17 +66,12 @@ const Rooms = ({ rooms, onRoomDeleted }) => {
                   {room.room_difficulty}
                 </Chip>
               </div>
-              <div className="content-center">
-                <h1 className="text-2xl text-bold text-white text-center content-center">
-                  <Link
-                    href={`/teacher-dashboard/rooms/${room.room_code}`}
-                    className="hover:underline underline-offset-2"
-                  >
-                    {room.room_name}
-                  </Link>
+              <div className="flex w-full justify-center items-center text-center">
+                <h1 className="text-2xl text-bold text-white hover:underline">
+                  {room.room_name}
                 </h1>
               </div>
-            </div>
+            </CardHeader>
             <CardFooter className="row-span-2 grid grid-cols-2 justify-between bg-white">
               <div className="p-2 text-[#7469B6] flex items-center">
                 <p>Code: {room.room_code}</p>
