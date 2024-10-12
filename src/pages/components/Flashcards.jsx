e6bd93ebf2e0583593aabc33fea1ca41936375f8
MarkBit109 <markbit109@gmail.com>
2024-10-12 18:36:38 +0800

    Create page on thinckpic+ and color game

--- src/pages/components/Flashcards.jsx
+++ src/pages/components/Flashcards.jsx
@@ -18,6 +18,18 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "swiper/css/effect-creative";
+
+// import required modules
+import { Pagination, Navigation } from "swiper/modules";
+import { EffectCreative } from "swiper/modules";
 const handleTextToSpeech = (text) => {
   const utterance = new SpeechSynthesisUtterance(text);
   const synth = window.speechSynthesis;
@@ -60,63 +72,79 @@ const Flashcards = ({ flashcards }) => {
         <h1>Flashcards</h1>
       </div>
       <div className="flex flex-wrap gap-4">
-        <Carousel className="w-full drop-shadow-lg rounded-md">
-          <CarouselContent>
-            {flashcards.map((flashcard) => (
-              <CarouselItem key={flashcard.flashcard_id}>
-                <Card className="w-full h-[500px] rounded-md">
-                  {!showDescription[flashcard.flashcard_id] && (
-                    <CardBody className="flex flex-col justify-center items-center gap-4">
-                      <div className="flex text-2xl">
-                        <p>{flashcard.term}</p>
+        <Swiper
+          pagination={{
+            type: "progressbar",
+          }}
+          grabCursor={true}
+          effect={"creative"}
+          creativeEffect={{
+            prev: {
+              shadow: true,
+              translate: [0, 0, -400],
+            },
+            next: {
+              translate: ["100%", 0, 0],
+            },
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation, EffectCreative]}
+          className="mySwiper w-full drop-shadow-lg rounded-md items"
+        >
+          {flashcards.map((flashcard) => (
+            <SwiperSlide key={flashcard.flashcard_id}>
+              <Card className="w-full h-[500px] rounded-md">
+                {!showDescription[flashcard.flashcard_id] && (
+                  <CardBody className="flex flex-col justify-center items-center gap-4">
+                    <div className="flex text-2xl">
+                      <p>{flashcard.term}</p>
+                    </div>
+                  </CardBody>
+                )}
+                {showDescription[flashcard.flashcard_id] && (
+                  <CardBody className="w-full flex px-8 justify-center items-center flex-row gap-4 max-sm:flex-col max-sm:justify-center max-sm:py-4">
+                    {flashcard.image && (
+                      <div className="flex w-1/2 justify-center items-center">
+                        <img
+                          src={flashcard.image}
+                          alt={flashcard.term}
+                          className="h-auto w-full max-sm:w-1/2"
+                        />
                       </div>
-                    </CardBody>
-                  )}
-                  {showDescription[flashcard.flashcard_id] && (
-                    <CardBody className="w-full flex px-8 justify-center items-center flex-row gap-4 max-sm:flex-col max-sm:justify-center max-sm:py-4">
-                      {flashcard.image && (
-                        <div className="flex w-1/2 justify-center items-center">
-                          <img
-                            src={flashcard.image}
-                            alt={flashcard.term}
-                            className="h-auto w-full max-sm:w-1/2"
-                          />
-                        </div>
-                      )}
+                    )}
 
-                      <div className="flex w-full justify-center items-center text-2xl max-h-[300px] overflow-y-auto max-sm:w-full max-sm:text-lg max-sm:max-h-[150px]">
-                        <p>{flashcard.description}</p>
-                        {flashcard.audio && (
-                          <audio
-                            src={flashcard.audio}
-                            controls
-                            className="w-full"
-                          />
-                        )}
-                      </div>
-                    </CardBody>
-                  )}
-                  <CardFooter className="flex justify-center items-center pt-0 ">
-                    <Button
-                      isIconOnly
-                      radius="full"
-                      className="bg-[#7469B6] text-white border-0"
-                      onClick={() => toggleCardBody(flashcard.flashcard_id)}
-                    >
-                      {showDescription[flashcard.flashcard_id] ? (
-                        <RotateCcw size={20} />
-                      ) : (
-                        <RotateCw size={20} />
+                    <div className="flex w-full justify-center items-center text-2xl max-h-[300px] overflow-y-auto max-sm:w-full max-sm:text-lg max-sm:max-h-[150px]">
+                      <p>{flashcard.description}</p>
+                      {flashcard.audio && (
+                        <audio
+                          src={flashcard.audio}
+                          controls
+                          className="w-full"
+                        />
                       )}
-                    </Button>
-                  </CardFooter>
-                </Card>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious className="bg-[#7469B6] text-white border-0" />
-          <CarouselNext className="bg-[#7469B6] text-white border-0" />
-        </Carousel>
+                    </div>
+                  </CardBody>
+                )}
+                <CardFooter className="flex justify-center items-center pt-0 ">
+                  <Button
+                    isIconOnly
+                    radius="full"
+                    className="bg-[#7469B6] text-white border-0"
+                    onClick={() => toggleCardBody(flashcard.flashcard_id)}
+                  >
+                    {showDescription[flashcard.flashcard_id] ? (
+                      <RotateCcw size={20} />
+                    ) : (
+                      <RotateCw size={20} />
+                    )}
+                  </Button>
+                </CardFooter>
+              </Card>
+            </SwiperSlide>
+          ))}
+          {/* <CarouselPrevious className="bg-[#7469B6] text-white border-0" />
+          <CarouselNext className="bg-[#7469B6] text-white border-0" /> */}
+        </Swiper>
       </div>
     </div>
   );
