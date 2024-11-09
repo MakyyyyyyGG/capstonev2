b205e4dc9c34ccecda6173ebcb54987fe00b4778
MarkBit109 <markbit109@gmail.com>
2024-11-09 21:45:13 +0800

    Confetti Components

---
+++ src/pages/components/ConfettiCrossfire.jsx
@@ -0,0 +1,7 @@
+import Crossfire from "react-canvas-confetti/dist/presets/crossfire";
+
+function ConfettiCrossfire() {
+  return <Crossfire autorun={{ speed: 15 }} />;
+}
+
+export default ConfettiCrossfire;
---
+++ src/pages/components/ConfettiFirework.jsx
@@ -0,0 +1,7 @@
+import Fireworks from "react-canvas-confetti/dist/presets/fireworks";
+
+function ConfettiFirework() {
+  return <Fireworks autorun={{ speed: 3 }} />;
+}
+
+export default ConfettiFirework;
