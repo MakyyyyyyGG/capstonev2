8d5c19b68b9c78fcdc55b77c8d3ff719f0a3dedb
MarkBit109 <markbit109@gmail.com>
2024-11-23 00:58:00 +0800

    UI on create assignment

--- src/pages/create_assignment/index.jsx
+++ src/pages/create_assignment/index.jsx
@@ -14,7 +14,7 @@ import {
   Tab,
   DatePicker,
 } from "@nextui-org/react";
-import { Youtube, Upload, Trash } from "lucide-react";
+import { Youtube, Upload, Trash2 } from "lucide-react";
 import { now, getLocalTimeZone } from "@internationalized/date";
 
 import ReactPlayer from "react-player";
@@ -104,79 +104,162 @@ const Index = () => {
   };
 
   return (
-    <div className="min-w-[80rem]">
+    <div className="w-full flex flex-col gap-4 p-4 max-w-[80rem] mx-auto">
       <Toaster />
-      <h1>Create Assignment</h1>
-      <Card>
-        <CardHeader>
+      <div className="flex my-5 justify-between items-center text-3xl font-extrabold">
+        <h1>Create Assignment</h1>
+      </div>
+
+      <Card className="w-full border border-[#7469B6] rounded-md flex p-4">
+        {/* <CardHeader>
           <h1>Create Assignment</h1>
-        </CardHeader>
+        </CardHeader> */}
         <CardBody>
           <form onSubmit={handleSubmit}>
-            <Input label="Title" />
-            <Textarea label="Instruction (Optional)" />
-            <DatePicker
-              label="Due Date"
-              variant="bordered"
-              hideTimeZone
-              showMonthAndYearPickers
-              defaultValue={now(getLocalTimeZone())}
-            />
-            <div className="flex items-center space-x-4">
-              <h3>Attach</h3>
-              <Button
-                auto
-                isIconOnly
-                startContent={<Youtube />}
-                onClick={() => setShowModal({ ...showModal, video: true })}
+            <div className="flex flex-col gap-4">
+              <Input
+                label="Title"
+                radius="sm"
+                variant="bordered"
+                color="secondary"
+                classNames={{
+                  label: "",
+                  inputWrapper: "border-1 border-[#7469B6]",
+                }}
               />
-              <Button
-                auto
-                isIconOnly
-                startContent={<Upload />}
-                onClick={() => setShowModal({ ...showModal, image: true })}
+              <Textarea
+                label="Instruction (Optional)"
+                radius="sm"
+                variant="bordered"
+                color="secondary"
+                classNames={{
+                  label: "",
+                  inputWrapper: "border-1 border-[#7469B6]",
+                }}
+              />
+              <DatePicker
+                label="Due Date"
+                variant="bordered"
+                hideTimeZone
+                showMonthAndYearPickers
+                defaultValue={now(getLocalTimeZone())}
+                radius="sm"
+                color="secondary"
+                classNames={{
+                  inputWrapper: "border border-[#7469B6]",
+                }}
               />
             </div>
 
-            <div className="mt-4">
-              <h3>Uploaded Media:</h3>
-              <ul>
+            <div className="flex flex-col items-center space-x-4 mt-4 ">
+              <h3 className="text-left w-full">Attach</h3>
+              <div className="flex space-x-3">
+                <Button
+                  auto
+                  isIconOnly
+                  size="lg"
+                  radius="full"
+                  color="secondary"
+                  variant="bordered"
+                  className="border-1 border-[#7469B6]"
+                  startContent={<Youtube />}
+                  onClick={() => setShowModal({ ...showModal, video: true })}
+                />
+                <Button
+                  auto
+                  isIconOnly
+                  size="lg"
+                  radius="full"
+                  color="secondary"
+                  variant="bordered"
+                  className="border-1 border-[#7469B6]"
+                  startContent={<Upload />}
+                  onClick={() => setShowModal({ ...showModal, image: true })}
+                />
+              </div>
+            </div>
+
+            <div className="mt-8">
+              <ul className="flex flex-col gap-2">
                 {mediaList.map((media, index) => (
-                  <li key={index}>
+                  <li
+                    key={index}
+                    className="flex items-center relative border-1 border-gray-300 rounded-md pl-2"
+                  >
                     {media.type === "video" || media.name.includes("mp4") ? (
-                      <ReactPlayer url={media.content} controls width="200px" />
+                      <div className="flex gap-4 items-center">
+                        <ReactPlayer
+                          url={media.content}
+                          controls
+                          width="200px"
+                          height="100px"
+                        />
+                        <div className="capitalize text-[#6B7280]">
+                          {media.type}
+                        </div>
+                      </div>
                     ) : media.type === "link" ? (
-                      <a
-                        href={media.content}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        {media.content}
-                      </a>
+                      <div className="flex gap-4 items-center">
+                        <a
+                          href={media.content}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {media.content}
+                        </a>
+                        <div className="capitalize text-[#6B7280]">
+                          {media.type}
+                        </div>
+                      </div>
                     ) : media.type === "image" ? (
-                      <div>
-                        <p>{media.name}</p>
+                      <div className="flex gap-4 items-center">
                         {media.content.endsWith(".mp4") ? (
-                          <video src={media.content} controls width="200px" />
-                        ) : (
-                          <img
+                          <video
                             src={media.content}
-                            alt={media.name}
+                            controls
                             width="200px"
+                            height="100px"
                           />
+                        ) : (
+                          <div>
+                            <div className="bg-black object-contain w-[200px] h-[100px]">
+                              <img
+                                src={media.content}
+                                alt={media.name}
+                                className="object-contain w-[200px] h-[100px]"
+                              />
+                            </div>
+                          </div>
                         )}
+                        <div>
+                          <p>{media.name}</p>
+                          <div className="capitalize text-[#6B7280]">
+                            {media.type}
+                          </div>
+                        </div>
                       </div>
                     ) : null}
                     <Button
                       isIconOnly
-                      startContent={<Trash />}
+                      variant="light"
+                      color="danger"
+                      startContent={<Trash2 />}
                       onClick={() => handleDeleteMedia(index)}
+                      className="absolute right-4"
                     />
                   </li>
                 ))}
               </ul>
             </div>
-            <Button type="submit">Create</Button>
+            <Button
+              radius="sm"
+              size="lg"
+              type="submit"
+              color="secondary"
+              className="mt-4 w-full"
+            >
+              Create Assignment
+            </Button>
           </form>
         </CardBody>
       </Card>
@@ -185,28 +268,51 @@ const Index = () => {
       {showModal.video && (
         <Modal
           isOpen
+          size="lg"
           onClose={() => setShowModal({ ...showModal, video: false })}
         >
           <ModalContent>
             <ModalHeader>Attach Video</ModalHeader>
             <ModalBody>
-              <Tabs>
+              <Tabs fullWidth>
+                <Tab title="From URL">
+                  <div className="flex gap-2 mt-4">
+                    <Input
+                      variant="bordered"
+                      color="secondary"
+                      isClearable
+                      radius="sm"
+                      size="lg"
+                      placeholder="Enter YouTube URL"
+                      classNames={{
+                        label: "text-white",
+                        inputWrapper: "bg-[#ffffff] border-1 border-[#7469B6]",
+                      }}
+                      value={youtubeUrl}
+                      onChange={(e) => setYoutubeUrl(e.target.value)}
+                    />
+                    <Button
+                      onClick={() => handleAddUrl("video", youtubeUrl)}
+                      radius="sm"
+                      size="lg"
+                      color="secondary"
+                    >
+                      Add
+                    </Button>
+                  </div>
+                </Tab>
                 <Tab title="Upload">
                   <input
                     type="file"
                     accept="video/*"
                     onChange={(e) => handleFileUpload(e, "video")}
+                    class="block w-full text-sm text-slate-500 mt-4 
+                    file:mr-4 file:py-3 file:px-6
+                    file:rounded-md file:border-0
+                    file:text-base 
+                    file:bg-[#7828C8] file:text-white
+                    hover:file:bg-[#9353D3]"
                   />
-                </Tab>
-                <Tab title="URL">
-                  <Input
-                    label="Video URL"
-                    value={youtubeUrl}
-                    onChange={(e) => setYoutubeUrl(e.target.value)}
-                  />
-                  <Button onClick={() => handleAddUrl("video", youtubeUrl)}>
-                    Confirm
-                  </Button>
                 </Tab>
               </Tabs>
             </ModalBody>
@@ -218,28 +324,51 @@ const Index = () => {
       {showModal.image && (
         <Modal
           isOpen
+          size="lg"
           onClose={() => setShowModal({ ...showModal, image: false })}
         >
           <ModalContent>
             <ModalHeader>Attach Image</ModalHeader>
             <ModalBody>
-              <Tabs>
+              <Tabs fullWidth>
+                <Tab title="From URL">
+                  <div className="flex gap-2 mt-4">
+                    <Input
+                      variant="bordered"
+                      color="secondary"
+                      isClearable
+                      radius="sm"
+                      size="lg"
+                      placeholder="Enter Image URL"
+                      classNames={{
+                        label: "text-white",
+                        inputWrapper: "bg-[#ffffff] border-1 border-[#7469B6]",
+                      }}
+                      value={urlLink}
+                      onChange={(e) => setUrlLink(e.target.value)}
+                    />
+                    <Button
+                      onClick={() => handleAddUrl("image", urlLink)}
+                      radius="sm"
+                      size="lg"
+                      color="secondary"
+                    >
+                      Add
+                    </Button>
+                  </div>
+                </Tab>
                 <Tab title="Upload">
                   <input
                     type="file"
                     accept="image/*"
                     onChange={(e) => handleFileUpload(e, "image")}
+                    class="block w-full text-sm text-slate-500 mt-4 
+                    file:mr-4 file:py-3 file:px-6
+                    file:rounded-md file:border-0
+                    file:text-base 
+                    file:bg-[#7828C8] file:text-white
+                    hover:file:bg-[#9353D3]"
                   />
-                </Tab>
-                <Tab title="URL">
-                  <Input
-                    label="Image URL"
-                    value={urlLink}
-                    onChange={(e) => setUrlLink(e.target.value)}
-                  />
-                  <Button onClick={() => handleAddUrl("image", urlLink)}>
-                    Confirm
-                  </Button>
                 </Tab>
               </Tabs>
             </ModalBody>
