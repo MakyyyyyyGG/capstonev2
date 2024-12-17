b8d38ea9116d5995693df416e7652dc080c023a0
MarkyyyyyyGG <clerigom7@gmail.com>
2024-12-17 15:57:45 +0800

    added live chat support

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,7 +8,7 @@ import { SessionProvider, useSession } from "next-auth/react";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import SidebarStudent from "./components/SidebarStudent";
-// Import the Inter font
+
 const inter = Inter({ subsets: ["latin"] });
 const balsamiqSans = Balsamiq_Sans({
   subsets: ["latin"],
@@ -32,13 +32,12 @@ function AppContent({ Component, pageProps }) {
 
   const fontClass =
     session && session.user.role === "student"
-      ? balsamiqSans.className // ? balsamiqSans.className (For Balsamiq Sans)
+      ? balsamiqSans.className
       : inter.className;
 
   return (
     <>
       <style jsx global>{`
-        /* Apply the Inter font to the whole application */
         html,
         body {
           font-family: ${inter.style.fontFamily}, sans-serif;
@@ -70,6 +69,38 @@ function AppContent({ Component, pageProps }) {
           <Component {...pageProps} />
         )}
       </div>
+
+      {/* Drift Script */}
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          "use strict";
+          !function() {
+            var t = window.driftt = window.drift = window.driftt || [];
+            if (!t.init) {
+              if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+              t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
+              t.factory = function(e) {
+                return function() {
+                  var n = Array.prototype.slice.call(arguments);
+                  return n.unshift(e), t.push(n), t;
+                };
+              }, t.methods.forEach(function(e) {
+                t[e] = t.factory(e);
+              }), t.load = function(t) {
+                var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+                o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+                var i = document.getElementsByTagName("script")[0];
+                i.parentNode.insertBefore(o, i);
+              };
+            }
+          }();
+          drift.SNIPPET_VERSION = '0.3.1';
+          drift.load('7sr48xa2zh7y');
+        `,
+        }}
+      />
     </>
   );
 }
